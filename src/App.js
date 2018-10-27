b72c1f5bef5025d853d3795c06a3a0d4fd92fc1c
leslie-alldridge <leslie.alldridge@gmail.com>
2018-10-27 16:06:30 +1300

    mobile cards add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import NavbarTop from "./Components/Navbar";
+import Portfolio from "./Components/Portfolio";
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <NavbarTop />
         <Header />
+        <Portfolio />
       </div>
     );
   }
