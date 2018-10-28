1b80f913527f2a3220f170a7f2eb371a21d82404
leslie-alldridge <leslie.alldridge@gmail.com>
2018-10-29 10:01:40 +1300

    contact card

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,33 +1,36 @@
 import React from 'react';
-import { Tab, Row, Col, Nav, NavItem } from 'react-bootstrap';
-
-import TechBlogs from './TechBlogs';
-import PersonalBlogs from './PersonalBlogs';
+import { Media, Row, Col, Image } from 'react-bootstrap';
 
 export default function Contact(props) {
   return (
-    <div>
-      <Tab.Container id="center-tabs" defaultActiveKey="first">
-        <Row className="clearfix">
-          <Col sm={4} md={6} lg={12}>
-            <Nav id="navBlog" bsStyle="pills">
-              <NavItem eventKey="first">Personal Blogs</NavItem>
-              <NavItem eventKey="second">Technical Blogs</NavItem>
-            </Nav>
-          </Col>
+    <div class="container">
+      <Media id="contactCard">
+        <Media.Left align="top">
+          <img
+            className="img-circle"
+            responsive
+            width={128}
+            height={128}
+            src="/pimg.jpg"
+            alt="thumbnail"
+          />
+        </Media.Left>
+        <Media.Body>
+          <Media.Heading>Top aligned media</Media.Heading>
+          <p>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+            scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum
+            in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
+            nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+          </p>
 
-          <Col sm={4} md={6} lg={12}>
-            <Tab.Content animation>
-              <Tab.Pane eventKey="first">
-                <PersonalBlogs />
-              </Tab.Pane>
-              <Tab.Pane eventKey="second">
-                <TechBlogs />
-              </Tab.Pane>
-            </Tab.Content>
-          </Col>
-        </Row>
-      </Tab.Container>
+          <p>
+            Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu
+            leo. Cum sociis natoque penatibus et magnis dis parturient montes,
+            nascetur ridiculus mus.
+          </p>
+        </Media.Body>
+      </Media>
     </div>
   );
 }
