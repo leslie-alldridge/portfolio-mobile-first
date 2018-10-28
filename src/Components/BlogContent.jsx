1f7141fa0f49fe7ee5811793ea21e414697f7bee
leslie-alldridge <leslie.alldridge@gmail.com>
2018-10-29 09:14:31 +1300

    nav centered

--- src/Components/BlogContent.jsx
+++ src/Components/BlogContent.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { Tab, Row, Col, Nav, NavItem } from "react-bootstrap";
-import TechBlogs from "./TechBlogs";
+import React from 'react';
+import { Tab, Row, Col, Nav, NavItem } from 'react-bootstrap';
+import TechBlogs from './TechBlogs';
 export default function BlogContent(props) {
   return (
     <div>
       <Tab.Container id="center-tabs" defaultActiveKey="first">
         <Row className="clearfix">
           <Col sm={4} md={6} lg={12}>
-            <Nav bsStyle="pills">
+            <Nav id="navBlog" bsStyle="pills">
               <NavItem eventKey="first">Personal Blogs</NavItem>
               <NavItem eventKey="second">Technical Blogs</NavItem>
             </Nav>
